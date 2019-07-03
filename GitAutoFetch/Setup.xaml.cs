fd1742207a0eda25d4c9183b2887e7e49473c0e9
Vinicius Alberkovics <viniciusa.d@hotmail.com>
2019-07-02 22:47:27 -0300

    Fix in vsix manifest, add Setup.xaml

---
+++ GitAutoFetch/Setup.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GitAutoFetch
+{
+    /// <summary>
+    /// Interaction logic for Setup.xaml.
+    /// </summary>
+    [ProvideToolboxControl("GitAutoFetch.Setup", true)]
+    public partial class Setup : UserControl
+    {
+        public Setup()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnRestore_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
